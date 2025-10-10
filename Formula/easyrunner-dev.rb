d99da627e09af831d8bb1d76094068386fe719ff
Janaka Abeywardhana <contact@janaka.co.uk>
2025-10-11 00:20:17 +0100

    chore: update easyrunner-dev to 0.6.0.dev23+6fd38e3
    Development release from feat/add-continuous-delivery

--- Formula/easyrunner-dev.rb
+++ Formula/easyrunner-dev.rb
@@ -3,9 +3,9 @@ class EasyrunnerDev < Formula
 
   desc "Development builds of EasyRunner - Single server self-hosting PaaS"
   homepage "https://github.com/janaka/easyrunner"
-  url "https://github.com/janaka/easyrunner/releases/download/v0.6.0.dev22+0c322a3/easyrunner_cli-0.6.0.dev22+0c322a3-py3-none-any.whl"
-  sha256 "a8c5be8dfc659d93d9958d62b0528162fe56a0678a81dbc6326a2160b450516c"
-  version "0.6.0.dev22+0c322a3"
+  url "https://github.com/janaka/easyrunner/releases/download/v0.6.0.dev23+6fd38e3/easyrunner_cli-0.6.0.dev23+6fd38e3-py3-none-any.whl"
+  sha256 "ac0f87ecbdaff296f897c8515def3a9b912d0a897d872ddc81d96e371fce8295"
+  version "0.6.0.dev23+6fd38e3"
 
   depends_on "python@3.11"
 
