b4dd85d893a6a305a623ed36308721c02ec6879e
Janaka A <contact@janaka.co.uk>
2025-10-11 00:13:41 +0100

    chore: add initial easyrunner-dev formula
    This is a placeholder formula that will be updated by CI/CD.
    The SHA256 and version will be updated on the first automated run.

---
+++ Formula/easyrunner-dev.rb
@@ -0,0 +1,19 @@
+class EasyrunnerDev < Formula
+  include Language::Python::Virtualenv
+
+  desc "Development builds of EasyRunner - Single server self-hosting PaaS"
+  homepage "https://github.com/janaka/easyrunner"
+  url "https://github.com/janaka/easyrunner/releases/download/v0.6.0.dev21-c7f8020/easyrunner_cli-0.6.0.dev21+c7f8020-py3-none-any.whl"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "0.6.0.dev21+c7f8020"
+
+  depends_on "python@3.11"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system bin/"er", "--version"
+  end
+end
