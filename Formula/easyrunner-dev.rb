02caeed4c419ac39c5c26a69398f1a9ab09ae413
Janaka Abeywardhana <contact@janaka.co.uk>
2025-10-11 12:25:10 +0100

    chore: update easyrunner-dev to 0.6.0.dev25+a30dcf5
    Development release from chore/fix-linter-errors

--- Formula/easyrunner-dev.rb
+++ Formula/easyrunner-dev.rb
@@ -3,9 +3,9 @@ class EasyrunnerDev < Formula
 
   desc "Development builds of EasyRunner - Single server self-hosting PaaS"
   homepage "https://github.com/janaka/easyrunner"
-  url "https://github.com/janaka/easyrunner/releases/download/v0.6.0.dev24+352e4c7/easyrunner_cli-0.6.0.dev24+352e4c7-py3-none-any.whl"
-  sha256 "83b3f5f2669eb3954b9dedbc76b5c0abd44013cf4830a8e16531016008833ecd"
-  version "0.6.0.dev24+352e4c7"
+  url "https://github.com/janaka/easyrunner/releases/download/v0.6.0.dev25+a30dcf5/easyrunner_cli-0.6.0.dev25+a30dcf5-py3-none-any.whl"
+  sha256 "d3b59696996be205e4135fb447a3573a6d2ee5a79a2f981961a3d8aa63ab0442"
+  version "0.6.0.dev25+a30dcf5"
 
   depends_on "python@3.11"
 
