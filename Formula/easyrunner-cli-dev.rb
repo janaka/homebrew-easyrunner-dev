7987c7ceb84611a6c82df2760eb9a1bc5d23b67b
Janaka A <contact@janaka.co.uk>
2025-10-12 21:32:01 +0100

    chore: manual update to v0.0.8.dev70+8c56d24 (version scheme migration fix)

--- Formula/easyrunner-cli-dev.rb
+++ Formula/easyrunner-cli-dev.rb
@@ -3,9 +3,9 @@ class EasyrunnerCliDev < Formula
 
   desc "Development builds of EasyRunner - Single server self-hosting PaaS"
   homepage "https://github.com/janaka/easyrunner"
-  url "https://github.com/janaka/easyrunner/releases/download/v0.6.0.dev46+eac6509/easyrunner_cli-0.6.0.dev46+eac6509-py3-none-any.whl"
-  sha256 "9bd7592ecde1f2396e121bd3058a7cf62da64dd2c15c5c65b4ecac501dfc067e"
-  version "0.6.0.dev46+eac6509"
+  url "https://github.com/janaka/easyrunner/releases/download/v0.0.8.dev70+8c56d24/easyrunner_cli-0.0.8.dev70+8c56d24-py3-none-any.whl"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.0.8.dev70+8c56d24"
 
   depends_on "python@3.13"
 
