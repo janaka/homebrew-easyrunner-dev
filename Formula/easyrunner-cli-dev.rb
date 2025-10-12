ca5b1702d8e47a30d3f58f758c0cbb2f9bdf3dc7
Janaka Abeywardhana <contact@janaka.co.uk>
2025-10-12 23:18:20 +0100

    chore: update easyrunner-cli-dev to 0.0.8.dev72+e2f2252
    Development release from refactor/cli-menu-options

--- Formula/easyrunner-cli-dev.rb
+++ Formula/easyrunner-cli-dev.rb
@@ -3,9 +3,9 @@ class EasyrunnerCliDev < Formula
 
   desc "Development builds of EasyRunner - Single server self-hosting PaaS"
   homepage "https://github.com/janaka/easyrunner"
-  url "https://github.com/janaka/easyrunner/releases/download/v0.0.8.dev71+554e293/easyrunner_cli-0.0.8.dev71+554e293-py3-none-any.whl"
-  sha256 "b57799eda8488cff72f9d60bd5c9d80ed83a2fd0f0a5ef454f0003bf93687649"
-  version "0.0.8.dev71+554e293"
+  url "https://github.com/janaka/easyrunner/releases/download/v0.0.8.dev72+e2f2252/easyrunner_cli-0.0.8.dev72+e2f2252-py3-none-any.whl"
+  sha256 "a65623c1dcd51158c31fe8adf98be0eab8e7ca254dba5dbae8d2fc4281612f42"
+  version "0.0.8.dev72+e2f2252"
 
   depends_on "python@3.13"
 
