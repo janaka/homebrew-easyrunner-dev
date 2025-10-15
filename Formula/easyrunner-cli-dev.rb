54393f888ba5fac626213fd7305263184294fc16
Janaka A <contact@janaka.co.uk>
2025-10-15 12:53:07 +0100

    chore: manual update to 0.0.8.dev93 to fix version format issue

--- Formula/easyrunner-cli-dev.rb
+++ Formula/easyrunner-cli-dev.rb
@@ -3,9 +3,9 @@ class EasyrunnerCliDev < Formula
 
   desc "Development builds of EasyRunner - Single server self-hosting PaaS"
   homepage "https://github.com/janaka/easyrunner"
-  url "https://github.com/janaka/easyrunner/releases/download/v0.0.8.dev74%2B95e7890/easyrunner_cli-0.0.8.dev74+95e7890-py3-none-any.whl"
-  sha256 "143a93f79b44adfe4f0364b9592ad94c42ddc57922cd1911f3c096584cbbd6c5"
-  version "0.0.8.dev74+95e7890"
+  url "https://files.pythonhosted.org/packages/3e/cc/0bcac58d187e16c394c82db70d5e77efd83282c445861f32a6ccd19c3b71/easyrunner_cli-0.0.8.dev93-py3-none-any.whl"
+  sha256 "4415fd2723df51f498b06a8a53ed473a4f64e90c17583555451c448f2a3ce865"
+  version "0.0.8.dev93"
 
   depends_on "python@3.13"
 
