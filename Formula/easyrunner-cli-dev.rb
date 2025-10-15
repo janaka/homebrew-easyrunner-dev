c9d66cddb810e3e718874f54f038373baef4795b
Janaka Abeywardhana <contact@janaka.co.uk>
2025-10-15 22:19:51 +0100

    chore: update easyrunner-cli-dev to 0.0.8.dev96
    Development release from refactor/cli-menu-options
    Source: PyPI

--- Formula/easyrunner-cli-dev.rb
+++ Formula/easyrunner-cli-dev.rb
@@ -3,9 +3,9 @@ class EasyrunnerCliDev < Formula
 
   desc "Development builds of EasyRunner - Single server self-hosting PaaS"
   homepage "https://github.com/janaka/easyrunner"
-  url "https://files.pythonhosted.org/packages/0c/2d/2dab54a792b40babd7f96ae5efac8bacadf345a766548076027438c6176b/easyrunner_cli-0.0.8.dev95-py3-none-any.whl"
-  sha256 "56525eaf5c5c9bc008499789fe3d61afe06bd049f6f0496d63c82f33613f1d33"
-  version "0.0.8.dev95"
+  url "https://files.pythonhosted.org/packages/82/9f/4e363d7ca667d9914b33e82a295aadd0109b422090efeb5a97840ad8a133/easyrunner_cli-0.0.8.dev96.tar.gz"
+  sha256 "a19a4063303a374a69403541e4b051b86234471aabaed9c02a9c64268bf0d701"
+  version "0.0.8.dev96"
 
   depends_on "python@3.13"
 
