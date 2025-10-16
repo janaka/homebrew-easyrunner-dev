7b802a8212e19be760e462d313af64296a5772dd
Janaka Abeywardhana <contact@janaka.co.uk>
2025-10-16 01:16:33 +0100

    chore: update easyrunner-cli-dev to 0.0.8.dev100
    Development release from refactor/auth-cmd-to-link
    Source: PyPI

--- Formula/easyrunner-cli-dev.rb
+++ Formula/easyrunner-cli-dev.rb
@@ -3,9 +3,9 @@ class EasyrunnerCliDev < Formula
 
   desc "Development builds of EasyRunner - Single server self-hosting PaaS"
   homepage "https://github.com/janaka/easyrunner"
-  url "https://files.pythonhosted.org/packages/79/de/cc7d51312d1b9990a6f1289edc453e67c1065f50ab891d6c9c432adfa5c8/easyrunner_cli-0.0.8.dev99.tar.gz"
-  sha256 "862eea2b9716cc5e90ea783015065e65b0e8ddb79c246e43141856c524af5d53"
-  version "0.0.8.dev99"
+  url "https://files.pythonhosted.org/packages/c3/af/d7306341d8c3ecd1a196da507dc85d4b7faff0170c095fefcc7d0b7416f6/easyrunner_cli-0.0.8.dev100.tar.gz"
+  sha256 "d42916f01c673c833e982ea76a7f0025b4707dd5abe9386de639aa31a1d111be"
+  version "0.0.8.dev100"
 
   depends_on "python@3.13"
 
